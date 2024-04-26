23a6a2966388274dc2261b10b76023b744061665
muhammadffa <muhammaddaffa463@gmail.com>
2024-04-26 14:09:28 +0700

    update syntax

--- express.ts
+++ express.ts
@@ -1,10 +1,18 @@
 import express from "express";
 
 const app = express();
+// const json = express.json()  
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send({message: "this is the main page"});
 });
+
+app.get('/user', (req, res) => {
+    res.json({  name:    "Daffa",
+                address: "Panggung",
+                country: "Indonesia",
+    });
+})
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
